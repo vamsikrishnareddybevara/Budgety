6e6b710c12b8be39dfab726fe46a6c176d4b0e18
vamsi <38369869+vamsikrishnareddybevara@users.noreply.github.com>
2019-01-10 19:47:54 +0530

    Added Eventlistener to the button

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Budget Controller
+
 let budgetController = ( function() {
 
 
@@ -6,6 +8,7 @@ let budgetController = ( function() {
 })();
 
 
+// UI Controller
 
 let UIController = ( function() {
 
@@ -16,10 +19,19 @@ let UIController = ( function() {
 })();
 
 
-
-
+// App Controller
 
 let controller = ( function( budgetCtrl, UICtrl) {
+	document.querySelector('click', function() {
+
+
+		
+	});
+	// 1.) Get the filled input data
+	// 2.) Add the item to the budget controller
+	// 3.) Add the item to the UI
+	// 4.) Calculate the budget
+	// 5.) Display the budget on the UI
 
 
 
