da5ca8bfa6a76e25683a7171166c854034176999
vamsi <38369869+vamsikrishnareddybevara@users.noreply.github.com>
2019-01-10 19:36:48 +0530

    Ui, Data, controller modules are initialized

---
+++ app.js
@@ -0,0 +1,27 @@
+let budgetController = ( function() {
+
+
+
+
+})();
+
+
+
+let UIController = ( function() {
+
+
+
+
+
+})();
+
+
+
+
+
+let controller = ( function( budgetCtrl, UICtrl) {
+
+
+
+
+})(budgetController, UIController);
