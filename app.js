ad51c5e0d4081e81d252b6f8185a1090eed6a9b9
vamsi <38369869+vamsikrishnareddybevara@users.noreply.github.com>
2019-01-10 19:59:03 +0530

    Added event Listener for keypress

--- app.js
+++ app.js
@@ -22,17 +22,25 @@ let UIController = ( function() {
 // App Controller
 
 let controller = ( function( budgetCtrl, UICtrl) {
-	document.querySelector('click', function() {
+	let ctrlAddItem = function() {
 
-
-		
-	});
 	// 1.) Get the filled input data
 	// 2.) Add the item to the budget controller
 	// 3.) Add the item to the UI
 	// 4.) Calculate the budget
 	// 5.) Display the budget on the UI
 
+
+	}
+
+
+
+	document.querySelector('click', ctrlAddItem);
+	document.querySelector('keypress', function(e) {
+		if(e.keyCode === 13 || e.which === 13) {
+			ctrlAddItem();
+		}
+	});
 
 
 
