ff180e7d6f3931be87f2861ef469bb12dab8a658
vamsi <38369869+vamsikrishnareddybevara@users.noreply.github.com>
2019-01-11 11:16:39 +0530

    Event initialization function created

--- app.js
+++ app.js
@@ -11,37 +11,57 @@ let budgetController = ( function() {
 // UI Controller
 
 let UIController = ( function() {
+	let DOMStrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputButton: '.add__btn'
+	}
+ 
 
 
-
-
-
+	return {
+		getInput : function() {
+			return {
+			type: document.querySelector(DOMStrings.inputType).value, // Will be either inc or exp
+			description: document.querySelector(DOMStrings.inputDescription).value,
+			value: document.querySelector(DOMStrings.inputValue).value 
+			};
+		},
+		getDOMStrings: function() {
+			return DOMStrings;
+		}
+	};
 })();
 
 
-// App Controller
+// App Contrller
 
 let controller = ( function( budgetCtrl, UICtrl) {
-	let ctrlAddItem = function() {
-
-	// 1.) Get the filled input data
-	// 2.) Add the item to the budget controller
-	// 3.) Add the item to the UI
-	// 4.) Calculate the budget
-	// 5.) Display the budget on the UI
-
-
-	}
-
-
-
-	document.querySelector('click', ctrlAddItem);
-	document.querySelector('keypress', function(e) {
-		if(e.keyCode === 13 || e.which === 13) {
+	let setupEventListeners = function() {
+		let DOM = UICtrl.getDOMStrings();
+		document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);	
+		document.addEventListener('keypress', function(e) {
+			if(e.keyCode === 13 || e.which === 13) {
 			ctrlAddItem();
+			}
+		});
+	};
+	let ctrlAddItem = function() {
+		// 1.) Get the filled input data
+		let input = UICtrl.getInput();
+		// 2.) Add the item to the budget controller
+		// 3.) Add the item to the UI
+		// 4.) Calculate the budget
+		// 5.) Display the budget on the UI
+	};	
+
+	return {
+		init: function() {
+			setupEventListeners();
 		}
-	});
-
-
+	};
 
 })(budgetController, UIController);
+console.log(controller);
+controller.init();
