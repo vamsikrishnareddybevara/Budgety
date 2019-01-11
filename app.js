6b3ed5e3792d337a85dddec419eacb010c5852d9
vamsi <38369869+vamsikrishnareddybevara@users.noreply.github.com>
2019-01-11 20:03:52 +0530

    Replace the place holder text with actual data

--- app.js
+++ app.js
@@ -84,6 +84,10 @@ let UIController = ( function() {
 			html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 		}
 			// Replace the place holder text with actual data
+
+			newHtml = html.replace('%id%', obj.id); 
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
 			// Insert the HTML into the DOM
 		},
 		getDOMStrings: function() {
