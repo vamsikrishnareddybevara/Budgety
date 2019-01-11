05fd7193e7f4c6d71a81fa15637efb33b608ec6b
vamsi <38369869+vamsikrishnareddybevara@users.noreply.github.com>
2019-01-11 19:55:17 +0530

    Create HTML string with placeholder

--- app.js
+++ app.js
@@ -48,10 +48,7 @@ let budgetController = ( function() {
 
 			// Return the element
 			return newItem;
-		},
-			testing: function() {
-				console.log(data);
-			}
+		}
 };
 
 
@@ -77,6 +74,15 @@ let UIController = ( function() {
 			description: document.querySelector(DOMStrings.inputDescription).value,
 			value: document.querySelector(DOMStrings.inputValue).value 
 			};
+		},
+		addListitem: function() {
+			// Create HTML string with placeholder
+
+			html = '<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+
+			html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			// Replace the place holder text with actual data
+			// Insert the HTML into the DOM
 		},
 		getDOMStrings: function() {
 			return DOMStrings;
